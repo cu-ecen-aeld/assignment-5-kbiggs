852cd14837dc0716c3f478a49b86375111abd23a
katie <katie@Ubuntu.myguest.virtualbox.org>
2024-02-07 07:24:43 -0700

    Save defconfig

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 004e6e64120f7591322d7572b56860a2d90224f9
+AESD_ASSIGNMENTS_VERSION = b4c96c23f9bf4d36ecf90c26a55d4445b5cab68f
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
